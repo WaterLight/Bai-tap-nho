52f01a7a8175fbd980ee2fd6120330da41c1f5c5
nam <namnh620@wru.vn>
2018-11-25 01:20:45 +0700

    Add count-time

--- js/main.js
+++ js/main.js
@@ -1,34 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	//phan code hiện nội dung ABCD
-	var nutA = document.querySelector('.nut-a');
-	var dapAnA = document.querySelector('.a');
-	nutA.addEventListener('click', function(){
-		dapAnA.classList.add('show');
-	});
-
-
-	var nutB = document.querySelector('.nut-b');
-	var dapAnB = document.querySelector('.b');
-	nutB.addEventListener('click', function(){
-		dapAnB.classList.add('show');
-	});
-
-	var nutC = document.querySelector('.nut-c');
-	var dapAnC = document.querySelector('.c');
-	nutC.addEventListener('click', function(){
-		dapAnC.classList.add('show');
-	});
-
-	var nutD = document.querySelector('.nut-d');
-	var dapAnD = document.querySelector('.d');
-	nutD.addEventListener('click', function(){
-		dapAnD.classList.add('show');
-	});
-	
-
-	//Phần check đáp án
-
 	var arrDapAn = ['d','b','b','b','c','b','c','b','d'];
 
 	var soThuTu = document.querySelector('.so-thu-tu');
@@ -62,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	});
 
-	var clock = document.querySelector('#clock');
+	var clock = document.querySelector('.overlay');
 	var nutBamGio = document.querySelector('.nut-h');
 	nutBamGio.addEventListener('click', function() {
 		var x = 9;
